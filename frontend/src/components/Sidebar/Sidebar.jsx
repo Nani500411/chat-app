e15ad71386c6001b9ef589001878514d0b5bdb0c
Nani <82075407+Nani500411@users.noreply.github.com>
2024-04-10 18:09:47 +0530

    some changes

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
-import Conversations from "./Conversations";
-import LogoutButton from "./LogoutButton";
-import SearchInput from "./SearchInput";
+import Conversations from "./Coversations";
+import LogoutButton from "./Logout";
+import SearchInput from "./Search";
 
 const Sidebar = () => {
 	return (
